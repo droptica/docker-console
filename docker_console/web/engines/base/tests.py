713938626ff69ef4ad46cf5263790140792868b6
Robert Kandzia <robert.kandzia@droptica.pl>
2018-09-19 13:51:36 +0200

    Fixed command test.

--- docker_console/web/engines/base/tests.py
+++ docker_console/web/engines/base/tests.py
@@ -31,24 +31,25 @@ class BaseTests(object):
             selenium_container_removed = True
 
         if not selenium_container_id or selenium_container_removed:
-            run_cmd('docker run -d --name selenium-test-%s %s %s %s'
-                    % (self.docker.base_alias, self.docker._get_links(), self.docker._get_hosts(),
+            run_cmd('docker run -d --name selenium-test-%s %s %s %s %s'
+                    % (self.docker.base_alias, self.docker.get_env_file(),
+                       self.docker._get_links(), self.docker._get_hosts(),
                        self.config.TESTS['IMAGES']['selenium_image'][0]))
             print "Waiting 5 sec."
             time.sleep(5)
 
         if parallel:
-            command_container_id = run_cmd('docker run -d %s %s %s %s -w %s %s codecept %s'
-                                           % (self.docker._get_volumes(), self.docker._get_links(),
-                                              self.docker._get_hosts(),
+            command_container_id = run_cmd('docker run -d %s %s %s %s %s -w %s %s codecept %s'
+                                           % (self.docker.get_env_file(), self.docker._get_volumes(),
+                                              self.docker._get_links(), self.docker._get_hosts(),
                                               link_selenium_name, os.path.join('/app', self.config.TESTS['LOCATION']),
                                               self.config.TESTS['IMAGES']['codecept_image'][0], cmd),
                                            return_output=True)
 
             return command_container_id
         else:
-            run_cmd('docker run --rm %s %s %s %s -w %s %s codecept %s'
-                    % (self.docker._get_volumes(), self.docker._get_links(), self.docker._get_hosts(),
+            run_cmd('docker run --rm %s %s %s %s %s -w %s %s codecept %s'
+                    % (self.docker.get_env_file(), self.docker._get_volumes(), self.docker._get_links(), self.docker._get_hosts(),
                        link_selenium_name, os.path.join('/app', self.config.TESTS['LOCATION']),
                        self.config.TESTS['IMAGES']['codecept_image'][0], cmd))
 
