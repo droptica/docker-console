a3d413858eec69506d2fec2adccba44f57158877
Daniel Kossakowski <daniel.kossakowski@droptica.pl>
2018-11-14 11:43:27 +0100

    Add --with-net switch

--- docker_console/__init__.py
+++ docker_console/__init__.py
@@ -22,6 +22,7 @@ args = ["-p", "--docker-run-path",
         "--suites",
         "--group-by",
         "--with-env",
+        "--with-net",
         "--with-aws-mounts",
         "-y"]
 
@@ -76,6 +77,9 @@ parser.add_option("--site", dest="drupal_site",
 parser.add_option("--with-env", action="store_true", dest="with_env",
               help="Use .env file with dcon commands")
 
+parser.add_option("--with-net", action="store_true", dest="with_net",
+              help="Use nginx-proxy network")
+
 parser.add_option("--with-aws-mounts", action="store_true", dest="with_aws_mounts",
               help="Attach AWS volumes to container")
 
@@ -88,6 +92,7 @@ parser.set_defaults(parallel_test_groups=5)
 parser.set_defaults(parallel_test_suites='')
 parser.set_defaults(parallel_group_by='files')
 parser.set_defaults(with_env=False)
+parser.set_defaults(with_net=False)
 parser.set_defaults(with_aws_mounts=False)
 
 group = OptionGroup(parser, "Available aliases", ", ".join('@%s' % alias for alias in available_aliases[:]))
